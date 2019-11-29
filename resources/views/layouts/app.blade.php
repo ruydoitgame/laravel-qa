3c88c7333a208d794874f0fa07a6973caf2c9230
ruydoitgame <ruydoitgame@gmail.com>
2019-11-29 16:02:27 +0700

    Apply bootstrap 4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
 </head>
 <body>
 <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+    <nav class="navbar navbar-expand-md navbar-light" style="background-color: #e3f2fd;">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
                 {{ config('app.name', 'Laravel') }}
