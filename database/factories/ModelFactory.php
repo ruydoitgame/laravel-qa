f083c0c1a89050c17b9d12965306f037c1b6eac5
ruydoitgame <ruydoitgame@gmail.com>
2019-11-30 15:07:25 +0700

    Generate fake answers

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -28,7 +28,16 @@ $factory->define(App\Question::class, function (Faker\Generator $faker) {
         'title' => rtrim($faker->sentence(rand(5,10)), "."),    //bỏ dấu . ở bên phải
         'body' => $faker->paragraph(rand(3,7), true),   //thay vì mãng thì thành chuỗi
         'views' => rand(0, 10),
-        'answers_count' => rand(0, 10),
+        //'answers_count' => rand(0, 10),
         'votes' => rand(-3, 10),
+    ];
+});
+
+$factory->define(App\Answer::class, function (Faker\Generator $faker) {
+    return [
+        'body' => $faker->paragraph(rand(3,7), true),   //thay vì mãng thì thành chuỗi
+        'user_id' => \App\User::pluck('id')->random(),
+        //'question_id' => \App\Question::pluck('id')->random(),
+        'votes_count' => rand(-3, 10),
     ];
 });
