3884232e24ebd1935dc6f563bcde7453d7eb1749
ruydoitgame <ruydoitgame@gmail.com>
2019-12-01 16:33:37 +0700

    Add vote to question

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,32 +5,43 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div class="card">
-                    <div class="card-header">
-                        <div class="d-flex align-items-center">
-                            <h1>{{$question->title}}</h1>
-                            <div class="ml-auto">
-                                <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Questions</a>
+                    <div class="card-body">
+                        <div class="card-title">
+                            <div class="d-flex align-items-center">
+                                <h1>{{$question->title}}</h1>
+                                <div class="ml-auto">
+                                    <a href="{{ route('questions.index') }}" class="btn btn-outline-secondary">Back to all Questions</a>
+                                </div>
                             </div>
                         </div>
-
-                    </div>
-
-                    <div class="card-body">
-                    {!!$question->body_html!!}
-                        <div class="float-right">
+                        <hr>
+                        <div class="media">
+                            <div class="d-flex flex-column vote-controls">
+                                <a href="" title="The question is useful" class="vote-up">^</a>
+                                <span class="votes-coute">1230</span>
+                                <a href="" title="The question is not useful" class="vote-down off">v</a>
+                                <a href="" title="Click to mark as favorite question (Click again to undo" class="favorite">Favorite
+                                </a>
+                            </div>
+                            <div class="media-body">
+                                {!!$question->body_html!!}
+                                <div class="float-right">
                                         <span class="text-muted">
                                             Answered {{ $question->created_date }}
                                         </span>
-                            <div class="media mt-2">
-                                <a href="{{$question->user->url}}" class="pr-2">
-                                    <img src="{{ $question->user->avatar }}" alt="avatar">
-                                </a>
-                                <div class="media-body mt-4">
-                                    <a href="{{$question->user->url}}">{{ $question->user->name }}</a>
+                                    <div class="media mt-2">
+                                        <a href="{{$question->user->url}}" class="pr-2">
+                                            <img src="{{ $question->user->avatar }}" alt="avatar">
+                                        </a>
+                                        <div class="media-body mt-4">
+                                            <a href="{{$question->user->url}}">{{ $question->user->name }}</a>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+
                 </div>
             </div>
         </div>
@@ -44,6 +55,13 @@
                         <hr>
                         @foreach($question->answers as $answer)
                             <div class="media">
+                                <div class="d-flex flex-column vote-controls">
+                                    <a href="" title="The question is useful" class="vote-up">^</a>
+                                    <span class="votes-coute">1230</span>
+                                    <a href="" title="The question is not useful" class="vote-down off">v</a>
+                                    <a href="" title="Mark this answer as best answer" class="">Accepted
+                                    </a>
+                                </div>
                                 <div class="media-body">
                                     {!!$answer->body_html !!}
                                     <div class="float-right">
